7cab6b8efc6371c99cbbf90d957db2394783ccbe
Gustavo Balbino <gu_gui14@hotmail.com>
2025-06-18 15:14:40 -0300

    (TabelaHash) Funcão Inserir

--- Models/TabelaHash.py
+++ Models/TabelaHash.py
@@ -1,27 +1,27 @@
-from ListaEncadeada import ListaEncadeada
-
 class TabelaHash:
     def __init__(self, tamanho=10):
         self.tamanho = tamanho # Tamanho máx de Funcionarios = 500, e Projetos = 2000
-        self.tabela = [ListaEncadeada() for _ in range(tamanho)] # Cada lista dentro da tabela, é equivalente a um 'bucket'
+        self.tabela = [[] for _ in range(tamanho)] # Cada lista dentro da tabela, é equivalente a um 'bucket'
+        '''
+        tabela = [ -> Tabela Hash
+            [],
+            [(chave1, valor1)], -> Bucket
+            []
+        ]
+        '''
     
     def inserir(self, chave, valor):
         # Recebe um par chave - valor, busca o endereço do elemento e insere os dados no endereço encontrado
         indice = self._funcao_hash(chave)
         bucket = self.tabela[indice]
         
-        if not bucket:
-            bucket.append((chave, valor)) # Valor deve ser o endereço do objeto, seja Funcionario ou Projeto
-            '''
-            tabela = [
-                item4 -> item1 -> item3 -> None,
-                None,
-                item2 -> None,
-                None
-            ]
-            '''
-            
-    
+        if bucket:
+            for elem in bucket:
+                if elem[0] == chave:
+                    return -1
+        
+        bucket.append((chave, valor)) # Valor deve ser o endereço do objeto, seja Funcionario ou Projeto
+
     def remover(self, chave):
         # Recebe uma chave, busca o endereço do elemento e altera seu status para "excluído"
         pass
