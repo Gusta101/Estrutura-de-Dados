2e162cc67333f64435ea82997661fbf98a1314e0
Gustavo Balbino <gu_gui14@hotmail.com>
2025-07-05 12:13:18 -0300

    (models) add class vetorlimitado

--- TabelaHash.py
+++ TabelaHash.py
@@ -75,28 +75,3 @@ class TabelaHash:
 
     def __str__(self):
         return "tabela = [\n    " + ",\n    ".join(str(self.tabela[i]) for i in range(self.tamanho)) + "\n]"
-
-# Testes
-func1 = Funcionario(123, "Alfredo", 15000)
-func2 = Funcionario(452, "Junior", 50000)
-func3 = Funcionario(3, "Julia", 2000)
-proj1 = Projeto("Projetinhopae", "10/05/2032", "01/02/0332", 10, 10000, 123)
-proj2 = Projeto("Nasa", "10/05/2032", "01/02/0332", 2, 100000, 452)
-proj3 = Projeto("SistemasDigitais", "10/05/2032", "01/02/0332", 5, 12000, 452)
-
-tableFunc = TabelaHash()
-tableFunc.inserir(func1)
-tableFunc.inserir(func2)
-tableFunc.inserir(func3)
-
-tableProj = TabelaHash()
-tableProj.inserir(proj1)
-tableProj.inserir(proj2)
-tableProj.inserir(proj3)
-
-tableFunc.remover(123)
-
-dados = {"nome": "Alexandre", "salario": 20000}
-tableFunc.alterar(123, dados)
-
-# print(tableFunc.tabela[3][0][1])
--- models.py
+++ models.py
@@ -22,3 +22,13 @@ class Projeto:
     def __str__(self):
         return f"Projeto: {self.nome}"
  
+class VetorLimitado:
+    def __init__(self, tamanho_max=10, vetor=[]):
+        self.tamanho_max = tamanho_max
+        self.vetor = vetor
+    
+    def append(self, dado):
+        if len(self.vetor) < self.tamanho_max:
+            self.vetor.append(dado)
+            return 1
+        return 0
